7d72b5a7e75cda669da77c080ae63f6344326d4c
[Brian Kim] <keepboy50@hotmail.com>
2016-05-04 21:16:57 +1200

    first time push

--- app/footer.js
+++ app/footer.js
@@ -4,7 +4,13 @@ var Footer = React.createClass({
     render: function() {
         return (
             <footer>
-                I'm footer
+                <ul>
+                	<li><a href="#home"><img src="../build/images/footer/footer_home_orange.png" /></a></li>
+                	<li><a href="#map"><img src="../build/images/footer/footer_maps_black.png" /></a></li>
+                	<li><a href="#time"><img src="../build/images/footer/footer_alarm_black.png" /></a></li>
+                	<li><a href="#fav"><img src="../build/images/footer/footer_favorite_black.png" /></a></li>
+                	<li><a href="#setting"><img src="../build/images/footer/footer_more_white.png" /></a></li>
+                </ul>
             </footer>
         );
     }
