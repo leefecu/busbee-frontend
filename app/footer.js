6f94c5b5e4fd58d8caff6ae4915c83b96d618424
[Brian Kim] <keepboy50@hotmail.com>
2016-05-04 21:27:23 +1200

    code changed

--- app/footer.js
+++ app/footer.js
@@ -7,7 +7,7 @@ var Footer = React.createClass({
                 <ul>
                 	<li><a href="#home"><img src="../build/images/footer/footer_home_orange.png" /></a></li>
                 	<li><a href="#map"><img src="../build/images/footer/footer_maps_black.png" /></a></li>
-                	<li><a href="#time"><img src="../build/images/footer/footer_alarm_black.png" /></a></li>
+                	<li><a href="#schedule"><img src="../build/images/footer/footer_alarm_black.png" /></a></li>
                 	<li><a href="#fav"><img src="../build/images/footer/footer_favorite_black.png" /></a></li>
                 	<li><a href="#setting"><img src="../build/images/footer/footer_more_white.png" /></a></li>
                 </ul>
