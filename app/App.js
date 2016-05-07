521f401e5a31c643aee8998655023ebb54dd5146
J LEE <ririellelee@gmail.com>
2016-05-08 01:10:41 +1200

    max width 768px coding _ not responsive yet

--- app/App.js
+++ app/App.js
@@ -1,16 +1,13 @@
 var React = require('react');
-var Header = require('./header.js');
-var SearchResult = require('./search.result.js');
+var MainSearch = require('./main_search.js');
 var Footer = require('./footer.js');
 
 var App = React.createClass({
     render: function() {
         return (
-            <div className="busbee">
-                <Header />
-                <img src="/images/kakao.jpg" />
-                <SearchResult />
-                <Footer />
+            <div className="busbee">               
+                <MainSearch />
+                
             </div>
         );
     }
---
+++ app/main_search.js
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var MainSearch = React.createClass({
+    render: function() {
+        return (            
+            <div className="main_con">
+                <div className="main_logo"> 
+                <h1>Real time<br/>bus finder</h1>            	  
+                <img src="../images/content/main_logo.png" /> 
+                <h2>auckland</h2>
+                </div>
+                <div className="main_search_title">
+                <img src="../images/content/bus_side.png" /> 
+                <p>Search Your Bus Information</p>
+                </div>
+                <div className="clear">
+                <div className="main_search">
+                <form> 
+                <input type="text" placeholder="Search..." />  
+                <img src="../images/content/main_search.png" />               
+                </form>
+                </div>
+            </div>
+           
+        );
+    }
+    
+});
+    
+module.exports = MainSearch;
