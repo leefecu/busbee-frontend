01831f301ab7a2c8124eb29a8f590140411a2b2b
Ken Lee <leefecu@gmail.com>
2016-05-07 18:00:21 +1200

    font folder

--- app/App.js
+++ app/App.js
@@ -9,6 +9,8 @@ var App = React.createClass({
             <div className="busbee">
                 <Header />
                 <img src="/images/kakao.jpg" />
+                <img src="/images/top/bin_black.png" />
+                <img src="/images/footer/footer_alarm_black.png" />
                 <SearchResult />
                 <Footer />
             </div>
