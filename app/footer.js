f79d05c6e03a53b36047e3a0e82c6ad2ace2ecc4
[Brian Kim] <keepboy50@hotmail.com>
2016-05-13 21:01:09 +1200

    Changed some codings in the footer which has to be changed from the following instructions

--- app/footer.js
+++ app/footer.js
@@ -5,11 +5,11 @@ var Footer = React.createClass({
         return (
             <footer>
                 <ul>
-                	<li><a href="#home"><img src="../build/images/footer/footer_home_orange.png" /></a></li>
-                	<li><a href="#map"><img src="../build/images/footer/footer_maps_black.png" /></a></li>
-                	<li><a href="#schedule"><img src="../build/images/footer/footer_alarm_black.png" /></a></li>
-                	<li><a href="#fav"><img src="../build/images/footer/footer_favorite_black.png" /></a></li>
-                	<li><a href="#setting"><img src="../build/images/footer/footer_more_white.png" /></a></li>
+                	<li><a href="#home"><img src="../images/footer/footer_home_orange.png" /></a></li>
+                	<li><a href="#map"><img src="../images/footer/footer_maps_black.png" /></a></li>
+                	<li><a href="#schedule"><img src="../images/footer/footer_alarm_black.png" /></a></li>
+                	<li><a href="#fav"><img src="../images/footer/footer_favorite_black.png" /></a></li>
+                	<li><a href="#setting"><img src="../images/footer/footer_more_white.png" /></a></li>
                 </ul>
             </footer>
         );
